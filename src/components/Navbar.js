0fa341316bb58de0aec41cdfbfe029564fe08d16
alibabatuhai <anshulagarwalla@gmail.com>
2022-09-15 11:44:50 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
+               <ul className="navbar-nav align-items-center">
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       MobileKart
+                    </Link>
+                 </li>
+               </ul>
+               <Link to="/cart" className="ml-auto">
+                  <ButtonContainer>
+                      <i className="fas fa-cart-plus"> Cart</i>
+                  </ButtonContainer>
+               </Link>
+            </NavWrapper>
+        )
+    }
+}
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3 rem;
+    text-transform:capitalize;
+}
+`;
---
+++ src/data.js
@@ -0,0 +1,98 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 50000,
+    company: "GOOGLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 60000,
+    company: "SAMSUNG",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 18000,
+    company: "htc",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Iphone 8",
+    img: "img/product-4.png",
+    price: 22000,
+    company: "Apple",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Samsung S20 Ultra",
+    img: "img/product-5.png",
+    price: 24000,
+    company: "Samsung",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Iphone 4",
+    img: "img/product-6.png",
+    price: 27000,
+    company: "apple",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30000,
+    company: "apple",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Iphone SE",
+    img: "img/product-8.png",
+    price: 20000,
+    company: "apple",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 50000,
+  company: "google",
+  inCart: false,
+  count: 0,
+  total: 0
+};
